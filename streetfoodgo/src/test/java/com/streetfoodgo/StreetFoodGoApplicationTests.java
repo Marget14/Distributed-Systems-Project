3e991a9f1f182e58b19b770f5c713582cd4757e7
Marget14 <154239774+Marget14@users.noreply.github.com>
2025-12-01 13:53:22 +0200

    Add files via upload

---
+++ streetfoodgo/src/test/java/com/streetfoodgo/StreetFoodGoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.streetfoodgo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StreetFoodGoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
