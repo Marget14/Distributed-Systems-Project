9b38f066bea97c7a2a625f3a7df998a0e8370cfe
Marget14 <154239774+Marget14@users.noreply.github.com>
2025-12-10 19:35:55 +0200

    Add files via upload

---
+++ src/main/java/com/streetfoodgo/config/SchedulingConfig.java
@@ -0,0 +1,11 @@
+package com.streetfoodgo.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+/**
+ * Scheduling configuration.
+ */
+@Configuration
+@EnableScheduling
+public class SchedulingConfig {}
