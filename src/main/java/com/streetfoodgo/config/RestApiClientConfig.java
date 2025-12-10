cb383d2343cd8e1c9377747bff399445a6f2bb7d
Marget14 <154239774+Marget14@users.noreply.github.com>
2025-12-10 22:05:15 +0200

    Add files via upload

--- src/main/java/com/streetfoodgo/config/RestApiClientConfig.java
+++ src/main/java/com/streetfoodgo/config/RestApiClientConfig.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package com.streetfoodgo.config;
 
 import org.springframework.context.annotation.Bean;
@@ -16,22 +15,3 @@ public class RestApiClientConfig {
         return new RestTemplate();
     }
 }
-=======
-package com.streetfoodgo.config;
-
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.client.RestTemplate;
-
-@Configuration
-public class RestApiClientConfig {
-
-    // @future Get me from application properties!
-    public static final String BASE_URL = "http://localhost:8081";
-
-    @Bean
-    public RestTemplate restTemplate() {
-        return new RestTemplate();
-    }
-}
->>>>>>> 9b38f066bea97c7a2a625f3a7df998a0e8370cfe
--- src/main/java/com/streetfoodgo/config/SchedulingConfig.java
+++ src/main/java/com/streetfoodgo/config/SchedulingConfig.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package com.streetfoodgo.config;
 
 import org.springframework.context.annotation.Configuration;
@@ -10,16 +9,3 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @Configuration
 @EnableScheduling
 public class SchedulingConfig {}
-=======
-package com.streetfoodgo.config;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.scheduling.annotation.EnableScheduling;
-
-/**
- * Scheduling configuration.
- */
-@Configuration
-@EnableScheduling
-public class SchedulingConfig {}
->>>>>>> 9b38f066bea97c7a2a625f3a7df998a0e8370cfe
