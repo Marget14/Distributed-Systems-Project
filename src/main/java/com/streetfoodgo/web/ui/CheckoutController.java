815b7bed490ee4d14207bf140332b70963776cbf
Marget14 <154239774+Marget14@users.noreply.github.com>
2026-01-07 02:42:45 +0200

    Add files via upload

---
+++ src/main/java/com/streetfoodgo/web/ui/CheckoutController.java
@@ -0,0 +1,38 @@
+package com.streetfoodgo.web.ui;
+
+import com.streetfoodgo.core.service.StoreService;
+import com.streetfoodgo.core.service.model.StoreView;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+/**
+ * Controller for checkout process.
+ */
+@Controller
+@RequestMapping("/checkout")
+public class CheckoutController {
+
+    private final StoreService storeService;
+
+    public CheckoutController(final StoreService storeService) {
+        if (storeService == null) throw new NullPointerException();
+        this.storeService = storeService;
+    }
+
+    @GetMapping
+    public String checkout(
+            @RequestParam(required = false) Long store,
+            final Model model) {
+
+        if (store != null) {
+            final StoreView storeView = this.storeService.getStore(store)
+                    .orElseThrow(() -> new IllegalArgumentException("Store not found"));
+            model.addAttribute("store", storeView);
+        }
+
+        return "checkout/checkout";
+    }
+}
