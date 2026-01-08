8e192da2d6b89ecfdec909e0bedb8f6c63b76ac2
Marget14 <154239774+Marget14@users.noreply.github.com>
2026-01-08 17:47:02 +0200

    Update ProfileController.java

--- src/main/java/com/streetfoodgo/web/ui/ProfileController.java
+++ src/main/java/com/streetfoodgo/web/ui/ProfileController.java
@@ -3,13 +3,11 @@ package com.streetfoodgo.web.ui;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
  * Controller for user profile page.
  */
 @Controller
-@RequestMapping("/profile")
 public class ProfileController {
 
     @GetMapping("/profile")
