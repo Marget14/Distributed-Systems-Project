b8435fde64a77982d6c840ec5272d4886f4ee6ed
Marget14 <154239774+Marget14@users.noreply.github.com>
2026-01-09 23:42:46 +0200

    Delete src/main/java/com/streetfoodgo/core directory

--- src/main/java/com/streetfoodgo/core/port/impl/dto/SendSmsResult.java
+++
@@ -1,6 +0,0 @@
-package com.streetfoodgo.core.port.impl.dto;
-
-/**
- * SendSmsResult DTO.
- */
-public record SendSmsResult(boolean sent) {}
--- src/main/java/com/streetfoodgo/core/service/impl/package-info.java
+++
@@ -1,1 +0,0 @@
-package com.streetfoodgo.core.service.impl;
