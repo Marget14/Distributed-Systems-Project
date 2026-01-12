a6a8970b98c97216e8fc2b9882e5cb0dd9a1dc2f
Marget14 <154239774+Marget14@users.noreply.github.com>
2026-01-12 03:47:58 +0200

    Delete src/main/java/com/streetfoodgo/core/repository/CartItemRepository.java

--- src/main/java/com/streetfoodgo/core/repository/CartItemRepository.java
+++
@@ -1,9 +0,0 @@
-package com.streetfoodgo.core.repository;
-
-import com.streetfoodgo.core.model.CartItem;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CartItemRepository extends JpaRepository<CartItem, Long> {
-}
