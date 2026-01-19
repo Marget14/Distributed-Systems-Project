96e57d92e170ffee40f6c7b54def77f09f119d79
Marget14 <154239774+Marget14@users.noreply.github.com>
2026-01-19 23:17:55 +0200

    Delete src/main/java/com/streetfoodgo/core/security directory

--- src/main/java/com/streetfoodgo/core/security/CurrentUser.java
+++
@@ -1,8 +0,0 @@
-package com.streetfoodgo.core.security;
-
-import com.streetfoodgo.core.model.PersonType;
-
-/**
- * @see CurrentUserProvider
- */
-public record CurrentUser(long id, String emailAddress, PersonType type) {}
