51c5eb2b2a3b9cc7f22404990c682887097d6799
Marget14 <154239774+Marget14@users.noreply.github.com>
2026-01-19 23:14:00 +0200

    Delete src/main/java/com/streetfoodgo/web/ui directory

--- src/main/java/com/streetfoodgo/web/ui/model/CompleteOrderForm.java
+++
@@ -1,9 +0,0 @@
-package com.streetfoodgo.web.ui.model;
-
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Size;
-
-public record CompleteOrderForm(
-    @NotNull @NotBlank @Size(max = 1000) String ownerContent
-) {}
