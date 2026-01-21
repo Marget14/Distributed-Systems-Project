9ff5c40aa6df176eeb2504dff2a66a6e6ec123b5
Marget14 <154239774+Marget14@users.noreply.github.com>
2026-01-21 04:35:26 +0200

    Delete src/main/java/com/streetfoodgo/core/service directory

--- src/main/java/com/streetfoodgo/core/service/model/StartOrderRequest.java
+++
@@ -1,6 +0,0 @@
-package com.streetfoodgo.core.service.model;
-
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Positive;
-
-public record StartOrderRequest(@NotNull @Positive Long id) {}
